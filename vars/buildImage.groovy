c1afd6c4c17fd7b091ebfbcaca0456fdfa8ba205
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-27 18:47:21 +0530

    Get parameter from Jenkinsfile script & use for dynamic values

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
-    echo "Building Docker image for Node application"
+def call(String ImageName) {
+    echo "Building Docker image for '$BRANCH_NAME' branch Node application"
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker build -t ${env.IMAGE_NAME} ."
+        sh "docker build -t $ImageName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push ${env.IMAGE_NAME}"
+        sh "docker push $ImageName"
     }
 }
