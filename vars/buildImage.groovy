64c19c8d370ba960696b611b94b71bb938b04d8d
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-27 20:40:31 +0530

    Fix code

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call(String ImageName) {
+def call() {
     echo "Building Docker image for shared lib Node application"
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
-        sh "docker build -t $ImageName ."
+        sh "docker build -t vikas1412/node-website:3.0 ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
-        sh "docker push $ImageName"
+        sh "docker push vikas1412/node-website:3.0"
     }
 }
