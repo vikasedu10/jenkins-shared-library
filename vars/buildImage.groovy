9b09754b97a3d33741b0933aeacfc55f3614f139
Vikas Bahuguna <vikasedu10@gmail.com>
2022-04-27 19:42:06 +0530

    Fix code

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def call(String ImageName) {
-    echo "Building Docker image for '$BRANCH_NAME' branch Node application"
+    echo "Building Docker image for 'shared lib' branch Node application"
     withCredentials([usernamePassword(credentialsId: 'dockerhub-credentials', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh "docker build -t $ImageName ."
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
